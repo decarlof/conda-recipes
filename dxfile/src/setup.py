a0c9f36c7929a9e30c2e1baa85a9f8b0185f38fb
Francesco De Carlo <decarlof@gmail.com>
2015-04-14 15:02:38 -0500

    added src

---
+++ dxfile/src/setup.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup(
+    author = 'David Vine Argonne National Laboratory',
+    description = 'Write data exchange files.',
+    py_modules = ['data_exchange'],
+    name = 'dxfile',
+    requires = (
+        'python',
+        'h5py',
+        ),
+    version = '1.4',
+)
